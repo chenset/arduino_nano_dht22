5593b61452364d75708f09ae82b369821e2f39aa
chenset <4199191@qq.com>
2016-11-13 13:31:58 +0800

    DHT22 test

--- arduino_nano_dht22.ino
+++ arduino_nano_dht22.ino
@@ -20,7 +20,7 @@ DHT22 myDHT22(6);
 void setup()
 {
    Wire.begin();           //I2C总线
-  Serial.begin(9600);
+//  Serial.begin(9600);
 
   
   display.begin(SSD1306_SWITCHCAPVCC);
@@ -31,7 +31,7 @@ void setup()
   display.print("GO");
   display.display();
 
-  delay(500);
+  delay(5000);
 
 }
 
@@ -39,30 +39,32 @@ void loop()
 {
   double *arr = getData();
   display.clearDisplay();
-  display.setTextSize(1);
+  display.setTextSize(2);
   display.setCursor(0, 0);
   
   display.println(arr[0]);
-   display.println(arr[1]);
+  display.println(arr[1]);
   display.display();
 
 
 
-Serial.println(*arr);
-Serial.println(arr[1]);
+//Serial.println(*arr);
+//Serial.println(arr[1]);
   delay(3000);
 }
 
 double* getData(){
-
   double *arr = new double[2];
-  arr[0]=1;
-  arr[1]=1;
-myDHT22.readData();
 
-    
-    Serial.println (myDHT22.getTemperatureCInt()/10);
-    Serial.println (abs(myDHT22.getTemperatureCInt()%10));
+  myDHT22.readData();
+
+  arr[0] = myDHT22.getTemperatureCInt()/10;
+  double temp1 = abs(myDHT22.getTemperatureCInt()%10);
+  arr[0]= arr[0]+(temp1/10);
+
+  arr[1] = myDHT22.getHumidityInt()/10;
+  double temp2 = abs(myDHT22.getHumidityInt()%10);
+  arr[1]= arr[1]+(temp2/10);
     
 //      sprintf(buf, "{\"temperature\": %hi.%01hi,\"humidity\" %i.%01i ,\"timestamp\":}",
 //                   myDHT22.getTemperatureCInt()/10, abs(myDHT22.getTemperatureCInt()%10),
